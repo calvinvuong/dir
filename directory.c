bbcd4252745c53312c0a74e926284d3e18880b45
Calvin Vuong <cvuong@stuy.edu>
2016-11-02 22:32:50 -0400

    finished

--- directory.c
+++ directory.c
@@ -17,17 +17,23 @@ int main() {
       printf("%s\n", entry->d_name);
     entry = readdir(folder);
   }
+  closedir(folder);
 
+  folder = opendir(".");
+  entry = readdir(folder);
   // print regular files
+  printf("\nRegular Files: \n");
   while ( entry != NULL ) {
     stat(entry->d_name, &buf);
     if ( entry->d_type != 0 && entry->d_type != 4 ) {
       printf("%s\n", entry->d_name);
       size += buf.st_size;
     }
+    entry = readdir(folder);
   }
 
-  printf("Total Directory Size: %d\n", size);
+  closedir(folder);
+  printf("\nTotal Directory Size: %d Bytes\n\n", size);
 
   return 0;
 }
